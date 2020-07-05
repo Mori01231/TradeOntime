6f93fd722bc4d8dc0f1bfcbf34c000322b872fa1
Mori01231 <mori01231mc@gmail.com>
2020-07-05 23:58:12 +0900

    [Add] Counter for how many tickets a player has in their inventory.

--- src/main/java/com/github/mori01231/tradeontime/OntimeTicketToOntime.java
+++ src/main/java/com/github/mori01231/tradeontime/OntimeTicketToOntime.java
@@ -1,12 +1,40 @@
 package com.github.mori01231.tradeontime;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class OntimeTicketToOntime implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return false;
+    }
+
+    //get amount of available slots in player inventory excluding armor and offhand
+    public int OntimeTickets(Player player){
+        //get player inventory.
+        Inventory inv = player.getInventory();
+
+        //initializing counter for slots.
+        int tickets=0;
+
+        //counting the number of available slots.
+        for (ItemStack item: inv.getContents()) {
+            try{
+                if(item.getItemMeta().getDisplayName().equals("§aオンタイムチケット")){
+                    tickets += item.getAmount();
+                }
+
+            }catch (Exception e){
+            }
+        }
+
+
+        //return the number of available slots.
+        return tickets;
     }
 }
