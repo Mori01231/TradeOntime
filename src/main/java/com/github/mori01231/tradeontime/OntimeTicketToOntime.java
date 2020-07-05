40ec1d96cddb83da502684411a219fa38957c912
Mori01231 <mori01231mc@gmail.com>
2020-07-05 23:39:34 +0900

    [Add] OntimeTicketToOntime executor

---
+++ src/main/java/com/github/mori01231/tradeontime/OntimeTicketToOntime.java
@@ -0,0 +1,12 @@
+package com.github.mori01231.tradeontime;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class OntimeTicketToOntime implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
--- src/main/java/com/github/mori01231/tradeontime/TradeOntime.java
+++ src/main/java/com/github/mori01231/tradeontime/TradeOntime.java
@@ -1,6 +1,5 @@
 package com.github.mori01231.tradeontime;
 
-//import org.black_ixx.playerpoints.PlayerPoints;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,29 +12,14 @@ public final class TradeOntime extends JavaPlugin {
     public static TradeOntime getInstance() {
         return instance;
     }
-/*
-    private PlayerPoints playerPoints;
 
-    // Validate that we have access to PlayerPoints
-    // @return True if we have PlayerPoints, else false.
-    private boolean hookPlayerPoints() {
-        final Plugin plugin = this.getServer().getPluginManager().getPlugin("PlayerPoints");
-        playerPoints = PlayerPoints.class.cast(plugin);
-        return playerPoints != null;
-    }
-    // Accessor for other parts of your plugin to retrieve PlayerPoints.
-    // @return PlayerPoints plugin instance
-    public PlayerPoints getPlayerPoints() {
-        return playerPoints;
-    }
-*/
 
     @Override
     public void onEnable() {
         getLogger().info("BetaTest has been enabled.");
         this.getCommand("tradeontime").setExecutor(new OntimeToOntimeTicket());
+        this.getCommand("tradeticket").setExecutor(new OntimeTicketToOntime());
 
-        //hookPlayerPoints();
 
         this.saveDefaultConfig();
     }
